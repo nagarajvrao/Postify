3c68e8d9c5d10a3ce83cee6e34ea60c27b6fb801
Nagaraj <nagaraj@backbase.com>
2024-05-25 08:32:30 +0530

    Initial Commit

---
+++ Postify/PostifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostifyApp.swift
+//  Postify
+//
+//  Created by Nagaraj Rao on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PostifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
